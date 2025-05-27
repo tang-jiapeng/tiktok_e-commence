031d298944486ceb3a43f3c4db54ff4d8fd00c7b
tang-jiapeng <3081129260@qq.com>
2025-05-27 13:30:53 +0800

    feat(user): implement get/update/delete user rpc service

--- app/user/biz/service/delete_user.go
+++ app/user/biz/service/delete_user.go
@@ -2,7 +2,11 @@ package service
 
 import (
 	"context"
-	user "tiktok_e-commerce/rpc_gen/kitex_gen/user"
+	"fmt"
+	"tiktok_e-commerce/app/user/biz/dao"
+	user_service "tiktok_e-commerce/rpc_gen/kitex_gen/user"
+
+	"github.com/cloudwego/kitex/pkg/klog"
 )
 
 type DeleteUserService struct {
@@ -13,8 +17,25 @@ func NewDeleteUserService(ctx context.Context) *DeleteUserService {
 }
 
 // Run create note info
-func (s *DeleteUserService) Run(req *user.DeleteUserReq) (resp *user.DeleteUserResp, err error) {
+func (s *DeleteUserService) Run(req *user_service.DeleteUserReq) (resp *user_service.DeleteUserResp, err error) {
 	// Finish your business logic.
 
-	return
+	userDAO := dao.GetUserDAO()
+	err = userDAO.Delete(req.UserId)
+	if err != nil {
+		klog.Error("删除用户失败:", err)
+		return &user_service.DeleteUserResp{
+			ResponseStatus: buildResponse("删除用户失败", false),
+		}, nil
+	}
+
+	// 同步删除redis缓存
+	cacheKey := fmt.Sprintf("user:%d", req.UserId)
+	if err := userDAO.Cache().Del(s.ctx, cacheKey).Err(); err != nil {
+		klog.Error("删除用户redis缓存失败:", err)
+	}
+	klog.Info("删除用户成功")
+	return &user_service.DeleteUserResp{
+		ResponseStatus: buildResponse("删除用户成功", true),
+	}, nil
 }
--- app/user/biz/service/get_user.go
+++ app/user/biz/service/get_user.go
@@ -2,7 +2,10 @@ package service
 
 import (
 	"context"
-	user "tiktok_e-commerce/rpc_gen/kitex_gen/user"
+	"tiktok_e-commerce/app/user/biz/dao"
+	user_service "tiktok_e-commerce/rpc_gen/kitex_gen/user"
+
+	"github.com/cloudwego/kitex/pkg/klog"
 )
 
 type GetUserService struct {
@@ -13,8 +16,31 @@ func NewGetUserService(ctx context.Context) *GetUserService {
 }
 
 // Run create note info
-func (s *GetUserService) Run(req *user.GetUserReq) (resp *user.GetUserResp, err error) {
+func (s *GetUserService) Run(req *user_service.GetUserReq) (resp *user_service.GetUserResp, err error) {
 	// Finish your business logic.
 
-	return
+	userDAO := dao.GetUserDAO()
+	user, err := userDAO.FindOne(s.ctx, req.UserId)
+	if err != nil {
+		klog.Error("用户不存在:", err)
+		return &user_service.GetUserResp{
+			ResponseStatus: buildResponse("用户不存在", false),
+		}, nil
+	}
+
+	if user == nil {
+		return &user_service.GetUserResp{
+			ResponseStatus: buildResponse("用户不存在", false),
+		}, nil
+	}
+
+	return &user_service.GetUserResp{
+		UserId:          user.UserId,
+		Email:           user.Email,
+		UserName:        user.Username,
+		UserPermissions: user.UserPermissions,
+		CreatedAt:       user.Created_at.Format("2006-01-02 15:04:05"),
+		UpdatedAt:       user.Updated_at.Format("2006-01-02 15:04:05"),
+		ResponseStatus:  buildResponse("获取用户信息成功", true),
+	}, nil
 }
