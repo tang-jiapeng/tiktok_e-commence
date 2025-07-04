36419a4c49aee75c9be57e90b962209e40f81c23
tang-jiapeng <3081129260@qq.com>
2025-07-04 19:33:11 +0800

    feat(order): Impl modify order status

--- app/order/biz/service/mark_order_paid.go
+++ app/order/biz/service/mark_order_paid.go
@@ -2,7 +2,13 @@ package service
 
 import (
 	"context"
+	"tiktok_e-commerce/common/constant"
+	"tiktok_e-commerce/order/biz/model"
 	order "tiktok_e-commerce/rpc_gen/kitex_gen/order"
+	"tiktok_e-commerce/user/biz/dal/mysql"
+
+	"github.com/cloudwego/kitex/pkg/klog"
+	"github.com/pkg/errors"
 )
 
 type MarkOrderPaidService struct {
@@ -14,7 +20,21 @@ func NewMarkOrderPaidService(ctx context.Context) *MarkOrderPaidService {
 
 // Run create note info
 func (s *MarkOrderPaidService) Run(req *order.MarkOrderPaidReq) (resp *order.MarkOrderPaidResp, err error) {
-	// Finish your business logic.
-
-	return
+	ctx := s.ctx
+	orderId := req.OrderId
+	rowAffected, err := model.MarkOrderPaid(ctx, mysql.DB, orderId)
+	if err != nil {
+		klog.CtxErrorf(ctx, "数据库修改订单状态失败,orderId:%s,err:%v", orderId, err)
+		return nil, errors.New("数据库修改订单状态失败")
+	}
+	if rowAffected == 0 {
+		return &order.MarkOrderPaidResp{
+			StatusCode: 3000,
+			StatusMsg:  constant.GetMsg(3000),
+		}, nil
+	}
+	return &order.MarkOrderPaidResp{
+		StatusCode: 0,
+		StatusMsg:  constant.GetMsg(0),
+	}, nil
 }
